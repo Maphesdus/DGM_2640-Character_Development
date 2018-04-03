9185c046896155beccdd56d745798623ea4de635
Unknown <nanobot18@gmail.com>
2018-04-03 13:25:13 -0600

    Unity project "State Machine Test" added.

---
+++ Fear Assignment/State Machine Test/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	GameObject player;
+
+	// START:
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// LATE UPDATE:
+	void LateUpdate () {
+		transform.position = player.transform.position;
+	}
+}
---
+++ Fear Assignment/State Machine Test/Assets/WASD_Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Movement : MonoBehaviour {
+	Animator anim;
+
+	int attackStateHash = Animator.StringToHash("Base Layer.Attack");
+	int idleStateHash = Animator.StringToHash("Base Layer.Idle");
+	int walkStateHash = Animator.StringToHash("Base Layer.Walk");
+
+	int LayerIndex = 0;
+
+	// START:
+	void Start () {
+		anim = GetComponent<Animator>();
+	} // END Start
+
+
+	// UPDATE:
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+
+		float move = Input.GetAxis("Vertical");
+		anim.SetFloat("Speed", move);
+
+		// Find out what state the animator is currently in:
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(LayerIndex);
+
+
+		// Previously we used "stateInfo.nameHash", but that parameter is obsolete in
+		// newer versions of Unity, so now we use "stateInfo.fullPathHash" instead.
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			//anim.SetTrigger (attackStateHash);
+			//anim.SetTrigger("Base Layer.Attack");
+			anim.SetBool("Jump", true);
+		}
+		else
+			anim.SetBool("Jump", false);
+			//float attack = Input.GetAxis("Fire1");
+			//anim.SetBool("Attack", attack);
+	} // END Update
+} // END Movement
